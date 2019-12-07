5b505e371a9e8eba88115344e7165b3cfb587e0b
soullnik <soullnik96@gmail.com>
2019-12-07 20:06:22 +0300

    dgd

--- src/components/WithGoogleMap/WithGoogleMap.js
+++ src/components/WithGoogleMap/WithGoogleMap.js
@@ -1,15 +1,38 @@
 import React from 'react'
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from "react-google-maps";
 
+// const MenuToggle = props => {
+    //     const cls = [
+    //         classes.MenuToggle,
+    //         'fa'
+    //     ]
+    //     if (props.isOpen) {
+    //         cls.push('fa-times')
+    //         cls.push(classes.open)
+    //     } else {
+    //         cls.push('fa-bars')
+    //     }
+    
+    
+    //     return (
+    //         <i
+    //         className={cls.join(' ')}
+    //         onClick={props.onToggle}
+    //         />
+    //     )
+    // }
 function Map() {
     return (
-    <GoogleMap 
+<div>
+<GoogleMap 
         defaultZoom={12} 
         disableDefaultUI
         defaultCenter={{lat: 53.904541, lng: 27.561523 }}
         defaultOptions={{
-            disableDefaultUI: true}} 
+            disableDefaultUI: true}}
         />
+    <Marker position={{lat: 53.904541, lng: 27.561523 }} onClick={() => console.log="Маркер" } />
+</div>
     );
 }
 
